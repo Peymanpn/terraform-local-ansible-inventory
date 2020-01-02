67e8cfe648075d6fd9c4bc8a6d3c49995381e624
Rob Stanford <2582028+orinokai@users.noreply.github.com>
2020-01-02 15:31:06 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,6 @@
 resource "local_file" "inventory" {
   content  = templatefile("${path.module}/inventory.tpl", { servers = var.servers })
   filename = "${var.output}/hosts"
-
-  provisioner "local-exec" {
-    command = "mkdir -p ${var.output}/secrets && gcloud auth print-access-token > ${var.output}/secrets/gcr_token"
-  }
 }
 
 resource "local_file" "secrets" {
